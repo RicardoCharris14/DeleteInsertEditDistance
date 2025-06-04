4a67ff86d7e79004d0a8db4fca3a3c0d4d9ef1d9
lulunkaii <sofyiaolate@gmail.com>
2025-06-04 01:16:17 -0400

    feat: Se implementa editDistanceDP

---
+++ editDistanceDP.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int editDistanceDP(std::string S, std::string T, int n, int m){
+    
+    std::vector<std::vector<int>> table(n + 1, std::vector<int>(m + 1, 0));
+    for (int i = 0; i < n + 1; i++){
+        for (int j = 0; j < m + 1; j++){
+            if (i == 0){
+                table[i][j] = j;
+                continue;
+            } else if (j == 0){
+                table[i][j] = i;
+                continue;
+            } else {
+                if (S[i-1] == T[j-1]){
+                    table[i][j] = table[i-1][j-1];
+                } else {
+                    table[i][j] = 1 + std::min(table[i-1][j], table[i][j-1]);
+                }
+            }
+        }
+    }
+    
+    return table[n][m];
+}
+
+
+
+int main(){
+    std::string S, T;
+    std::cin >> S >> T;
+    std::cout << editDistanceDP(S, T, S.length(), T.length());
+
+    return 0;
+}
